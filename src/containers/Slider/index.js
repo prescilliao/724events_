61bf01414a79f567aa18b744c66e16a6ba208f60
De Oliveira Prescillia <oliveiraprescillia04@gmail.com>
2024-06-20 19:32:17 +0200

    ajout de readonly dans input (ligne49)

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -46,6 +46,7 @@ const Slider = () => {
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
+                readOnly
                 />
               ))}
             </div>
